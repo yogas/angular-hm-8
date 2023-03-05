a723fe1c2f29cc889dea4eea5ff70d8a658cdfeb
Vladimir Shapovalov <vladimir.shapovalov@nimbusweb.co>
2023-03-05 14:05:03 +0700

    lesson 241

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,25 @@ export class AppComponent {
       instanceType: 'medium',
       name: 'Production Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date('2017-01-15')
     },
     {
       instanceType: 'large',
       name: 'User Database',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date('2017-01-15')
     },
     {
       instanceType: 'small',
       name: 'Development Server',
       status: 'offline',
-      started: new Date(15, 1, 2017)
+      started: new Date('2017-01-15')
     },
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date('2017-01-15')
     }
   ];
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
