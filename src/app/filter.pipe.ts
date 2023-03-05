4b01a4d9f20f05170f03bd985455054423156838
Vladimir Shapovalov <vladimir.shapovalov@nimbusweb.co>
2023-03-05 15:01:23 +0700

    lesson 247

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: unknown[], filterString: string, propName: string): unknown {
+    if (value.length === 0 || filterString.length === 0) {
+      return value;
+    }
+    return value.filter(item => {
+      const regExp = new RegExp(`^${filterString}`, 'g')
+      return item[propName].match(regExp)
+    })
+  }
+
+}
