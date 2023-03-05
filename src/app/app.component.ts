a7b0b1f3698f3d0011b0f55d55738861b06b63b7
Vladimir Shapovalov <vladimir.shapovalov@nimbusweb.co>
2023-03-05 15:23:09 +0700

    lesson 249

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise(resolve => setTimeout(() => resolve('stable'), 2000))
   filteredStatus = '';
 
   servers = [
